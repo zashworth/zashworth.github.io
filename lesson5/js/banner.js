3941ad002c2e766186a321ef6c48c638d1b46786
zashworth <ash13024@byui.edu>
2019-10-17 17:17:57 -0600

    Update

---
+++ lesson5/js/banner.js
@@ -0,0 +1,14 @@
+function showBanner() {
+/*var weekDay = new Date().getDay();*/
+var weekDay = 4;
+
+if(weekDay == 5) {
+    document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
+    document.getElementById("banner").style.padding = "6px"
+}
+else {
+
+}
+}
+
+showBanner()
