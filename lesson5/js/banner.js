4d5729efa78dbad6d556d63bcf246e1840c454c4
zashworth <ash13024@byui.edu>
2019-10-18 16:56:14 -0600

    Update

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,6 +1,5 @@
 function showBanner() {
-/*var weekDay = new Date().getDay();*/
-var weekDay = 4;
+var weekDay = new Date().getDay();
 
 if(weekDay == 5) {
     document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
