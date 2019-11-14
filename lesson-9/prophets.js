2748cddaa603dbaa2e4ef4c105ccd1b5dfa2735c
zashworth <ash13024@byui.edu>
2019-11-14 15:05:54 -0700

    Update

--- lesson-9/prophets.js
+++ lesson-9/prophets.js
@@ -8,7 +8,7 @@ fetch(requestURL)
   })
 
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
 
     const prophets = jsonObject['prophets'];
   
