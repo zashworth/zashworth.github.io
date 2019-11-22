715c91c26945226ca081e97316861f17d17b5fb2
zashworth <ash13024@byui.edu>
2019-11-21 20:44:36 -0700

    Update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,18 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=9e98a4bbd0cee37a9ef56989d1966f90&units=imperial";
+let town = document.getElementById('townName').innerHTML;
+let townID = "";
+if (town == "Preston Idaho") {
+  townID = "5604473"
+}
+else if (town == "Soda Springs Idaho") {
+  townID = "5607916"
+}
+else {
+  townID = "5585010"
+}
 
-const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=9e98a4bbd0cee37a9ef56989d1966f90&units=imperial";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=" + townID + "&APPID=9e98a4bbd0cee37a9ef56989d1966f90&units=imperial";
+
+const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=" + townID + "&APPID=9e98a4bbd0cee37a9ef56989d1966f90&units=imperial";
 
 fetch(apiURL)
   .then((response) => response.json())
@@ -8,32 +20,36 @@ fetch(apiURL)
     // console.log(jsObject);
 
     document.getElementById('currently').textContent = jsObject.weather[0].main;
-    document.getElementById('temp').textContent = jsObject.main.temp;
+    document.getElementById('temp').textContent = Math.round(jsObject.main.temp);
     document.getElementById('humid').textContent = jsObject.main.humidity;
-    document.getElementById('speed').textContent = jsObject.wind.speed;
+    document.getElementById('speed').textContent = Math.round(jsObject.wind.speed);
 
   });
 
   fetch(forecastURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    //console.log(jsObject);
 
     let tableRow = document.getElementsByTagName('td');
+    let t = 0;
+    
+    for (i = 0; i <= jsObject.list.length; i++) {
+      if (jsObject.list[i].dt_txt.substring(11, 19) == "18:00:00" && t < tableRow.length) {
+        let image = document.createElement('img');
+        let text = document.createElement('p');
+        let icon = jsObject.list[i].weather[0].icon;
+        let description = jsObject.list[i].weather[0].description;
+        text.innerHTML = jsObject.list[i].main.temp + " &#8457;";
+        image.setAttribute('src', "https://openweathermap.org/img/wn/" + icon + ".png");
+        image.setAttribute('alt', description);
+        tableRow[t].appendChild(image);
+        tableRow[t].appendChild(text);
 
-    for (i = 0; i <= tableRow.length; i++) {
-      tableRow[i].innerText = 74 ;
-      //tableRow[i].getElementsByTagName('img').setAttribute('src')
+
+        t++;
+      }
     }
-
-    document.getElementById('currently').textContent = jsObject.weather[0].main;
-    document.getElementById('temp').textContent = jsObject.main.temp;
-    document.getElementById('humid').textContent = jsObject.main.humidity;
-    document.getElementById('speed').textContent = jsObject.wind.speed;
-
-    /*const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);*/
+      
+    
   });
