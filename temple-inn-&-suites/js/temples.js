fb809f4718a9adcde37362211b6c0d23e864b7e3
zashworth <ash13024@byui.edu>
2019-12-19 18:02:51 -0700

    Update

--- temple-inn-&-suites/js/temples.js
+++ temple-inn-&-suites/js/temples.js
@@ -1,4 +1,4 @@
-const requestURL = "json/";
+const requestURL = "https://zashworth.github.io/temple-inn-&-suites/json/temple-info.json";
 
 
 
@@ -10,74 +10,49 @@ fetch(requestURL)
   .then(function (jsonObject) {
     // console.table(jsonObject);  // temporary checking for valid response and data parsing
 
-    const towns = jsonObject['towns'];
+    const temples = jsonObject['temples'];
   
-    for (let i = 0; i < towns.length; i++ ) {
-      if ( towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs') { 
-
+    for (let i = 0; i < temples.length; i++ ) {
+    
         let card = document.createElement('article');
         let text = document.createElement('section')
         let h2 = document.createElement('h2');
         let h5 = document.createElement('h5');
-        let yFound = document.createElement('p');
-        let population = document.createElement('p');
-        let rainFall = document.createElement('p');
+        let h6 = document.createElement('h5');
+        let h7 = document.createElement('h5');
+        let services = document.createElement('p');
+        let history = document.createElement('p');
+        let ordinances = document.createElement('p');
+        let sessions = document.createElement('p');
+        let closures = document.createElement('p');
         let image = document.createElement('img');
 
-        h2.textContent = towns[i].name;
-        h5.textContent = towns[i].motto;
-        yFound.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        population.textContent = 'Population: ' + towns[i].currentPopulation;
-        rainFall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall + 'in.';
-
-        image.setAttribute('src', 'images/' + towns[i].photo);
-        image.setAttribute('alt', towns[i].name);
+        h2.textContent = temples[i].location;
+        h5.textContent = temples[i].address;
+        h6.textContent = temples[i].telephone;
+        h7.textContent = temples[i].email;
+        
+        services.textContent = 'Services Offered: ' + temples[i].services;
+        history.textContent = 'Temple History: ' + temples[i].history;
+        ordinances.textContent = 'Ordinances Offered: ' + temples[i].ordinance;
+        sessions.textContent = 'Session Times: ' + temples[i].session;
+        closures.textContent = 'Temple Closures: ' + temples[i].closures;
+        image.setAttribute('src', 'images/' + temples[i].photo);
+        image.setAttribute('alt', temples[i].location);
         
         text.appendChild(h2);
         text.appendChild(h5);
-        text.appendChild(yFound);
-        text.appendChild(population);
-        text.appendChild(rainFall);
+        text.appendChild(h6);
+        text.appendChild(h7);
+        text.appendChild(services);
+        text.appendChild(history);
+        text.appendChild(ordinances);
+        text.appendChild(sessions);
+        text.appendChild(closures);
         card.appendChild(text);
         card.appendChild(image);
 
-        document.querySelector('div.cards').appendChild(card);
-    }
-    }
-
-  });
-
-  fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-
-    const towns = jsonObject['towns'];
-    const events = jsonObject['events'];
-  
-    for (let i = 0; i < towns.length; i++ ) {
-      if ( towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs') { 
-
-        let article = document.createElement('article');
-        let name = document.createElement('h3');
-
-        name.textContent = towns[i].name
-        document.getElementById('events').appendChild(article);
-        document.getElementById('events').appendChild(name);
-        
-        for (let j = 0; j < 4; j++) {
-          let event = document.createElement('p');
-          event.textContent = towns[i].events[j];
-          document.getElementById('events').appendChild(event);
-        }
-        
-
-        
-        
-    }
+        document.querySelector('div.temples').appendChild(card);
     }
 
   });
